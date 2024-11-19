dc93bd5839bf803f7b8ff2320aa5488dd745eb48
davey <daveymunters87@gmail.com>
2024-11-19 11:45:50 +0100

    Set up Tailwind CSS and project structure

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',       // HTML en JS in de src-map
+    './src/components/**/*.html'  // HTML-bestanden in components-map
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
